ea1abc9c80b1bb55ce21037bc414066200c89e2e
Jade Dominguez <plusjade@gmail.com>
2014-04-19 00:48:47 -0700

    What do the rules model?

--- data.js
+++ data.js
@@ -5,4 +5,17 @@ The rules I need to model:
     scissors cut paper
     paper covers rock
 
+A revised mockup of the rules in a more explicit format:
+
+    Choices (pairs)          Outcome
+    ----------------------------------------------
+    "rock" -> "scissors"    : "WIN"
+    "scissors" -> "rock"    : "LOSE"
+
+    "scissors" -> "paper"   : "WIN"
+    "paper" -> "scissors"   : "LOSE"
+
+    "paper" -> "rock"       : "WIN"
+    "rock" -> "paper"       : "LOSE"
+
 */
